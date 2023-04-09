96842511d6688b384d4576828c4d60f92b0f35ae
Ivan Anikin <ivan.anikin@outlook.com>
2023-04-09 16:37:23 +0200

    styling and video

--- static/js/frontend.js
+++ static/js/frontend.js
@@ -1,14 +1,45 @@
 
-console.log("Biatch");
 
-var video = document.querySelector("#videoElement");
+window.onload = () => {
+  
+  var video = document.querySelector("#videoElement");
+  
+  if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(function (stream) {
+        video.srcObject = stream;
+      })
+      .catch(function (err0r) {
+        console.log("Something went wrong!");
+      });
+  }
 
-if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function (stream) {
-      video.srcObject = stream;
-    })
-    .catch(function (err0r) {
-      console.log("Something went wrong!");
-    });
-}
+  
+	$('#sendbutton').click(() => {
+		imagebox = $('#imagebox')
+		input = $('#imageinput')[0]
+		if(input.files && input.files[0])
+		{
+			let formData = new FormData();
+			formData.append('image' , input.files[0]);
+			$.ajax({
+				url: "http://localhost:5000/test", // fix this to your liking
+				type:"POST",
+				data: formData,
+				cache: false,
+				processData:false,
+				contentType:false,
+				error: function(data){
+					console.log("upload error" , data);
+					console.log(data.getAllResponseHeaders());
+				},
+				success: function(data){
+					// alert("hello"); // if it's failing on actual server check your server FIREWALL + SET UP CORS
+					bytestring = data['status']
+					image = bytestring.split('\'')[1]
+					imagebox.attr('src' , 'data:image/jpeg;base64,'+image)
+				}
+			});
+		}
+	});
+};
